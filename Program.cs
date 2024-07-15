102ac66321bbfb8f3066649c74b6369e34f32306
Eason Lee <eason-lee@hotmail.co.uk>
2024-07-15 21:05:30 +0100

    Removed all irrelevant pages and set home page to the products page. Fixed edit submitting not working, forgot to add the Name section.

--- Program.cs
+++ Program.cs
@@ -31,7 +31,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Products}/{action=Index}/{id?}");
 
 app.Run();
 
