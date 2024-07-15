88fd5dbabee6d6c30983beb8f472bf4051e7a09d
Eason Lee <eason-lee@hotmail.co.uk>
2024-07-15 02:23:47 +0100

    Added the table categories and filled it with dummy data.

---
+++ Services/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingList.Models;
+
+namespace ShoppingList.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        
+        
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
